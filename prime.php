92d4d4d7e8d4eab283ba8a85c685ea8edd3286d1
Rali Dimitrov <webdev@localhost.org>
2020-02-21 10:40:47 +0200

    test

--- prime.php
+++ prime.php
@@ -1,8 +1,11 @@
 <?php
-//Enter your code here, enjoy!
-
+/**
+ * @var int $number [ the end number for iteration ]
+ * @var int $offset [ optional! Default value 2, represents the number from which to start the iteration ]
+ */
 function getPrimes($number) {
   $primes = "";
+  $test = -1;
   for($x = 2; $x < $number; $x++) {
     $flag = false;
     for($i = 2; $i < $x; $i++) {
@@ -13,9 +16,17 @@ function getPrimes($number) {
     }
     if( ($i == 2 || $i % 2 != 0 ) && !$flag ) {
       $primes .= $i . ", ";
+      if($number % $i == 0 && $i < $number) {
+        $test = $i;
+      }
+
     }
   }
-  return rtrim($primes, ", ");
+  echo "Selected number: " . $number;
+  echo "<hr>";
+  echo "The largest prime is: ",  $test;
+  echo "<hr>";
+  echo "List of primes until '{$number}': " . rtrim($primes, ", ");
 }
 
-print_r(getPrimes(100));
+getPrimes(20);
